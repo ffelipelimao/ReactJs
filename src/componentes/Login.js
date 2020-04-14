162c2ce1d10896a54f726cb75cf19f2968f87d9c
limao <lflimao@hotmail.com>
2020-04-14 19:36:39 -0300

    autenticacao com o firebase

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,37 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-const Login = props => {
-  return (
-    <div className="d-flex justify-content-center align-items-center container">
-      <form className="justify-content-center" id="cardfixlogin">
-        <div className="form-group">
-          <label for="exampleInputEmail1">Endereço de email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Seu email"
-          />
-          <small id="emailHelp" className="form-text text-muted">
-            Nunca vamos compartilhar seu email, com ninguém.
-          </small>
+import { auth } from "./../config_firebase";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.email = null;
+    this.senha = null;
+
+    this.state = {
+      isAuth: false,
+      isAuthing: false,
+      error: false,
+    };
+
+    this.autenticaUsuario = this.autenticaUsuario.bind(this);
+  }
+
+  autenticaUsuario() {
+    this.setState({ isAuthing: true, error: false });
+    auth
+      .signInWithEmailAndPassword(this.email.value, this.senha.value)
+      .then((user) => {
+        this.setState({ isAuth: true });
+      })
+      .catch((err) => {
+        this.setState({ error: true, isAuthing: false });
+      });
+  }
+
+  render() {
+    if (this.state.isAuth) {
+      return <Redirect to="/novofilme" />;
+    }
+    return (
+      <div className="d-flex justify-content-center align-items-center container">
+        <div className="justify-content-center" id="cardfixlogin">
+          <div className="form-group">
+            <label for="exampleInputEmail1">Endereço de email</label>
+            <input
+              type="email"
+              name="email"
+              ref={(ref) => (this.email = ref)}
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Seu email"
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Senha</label>
+            <input
+              type="password"
+              name="senha"
+              ref={(ref) => (this.senha = ref)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Senha"
+            />
+            {this.state.error && (
+              <small id="emailHelp" className="form-text text-muted">
+                Dados invalidos :(
+              </small>
+            )}
+          </div>
+          <button
+            type="button"
+            disabled={this.state.isAuthing}
+            className="btn btn-primary"
+            onClick={this.autenticaUsuario}
+          >
+            Enviar
+          </button>
         </div>
-        <div className="form-group">
-          <label for="exampleInputPassword1">Senha</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Senha"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Enviar
-        </button>
-      </form>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default Login;
--- src/componentes/Novo.js
+++ src/componentes/Novo.js
@@ -1,73 +1,104 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-const Novo = props => {
-  return (
-    <div
-      className="d-flex justify-content-right align-items-center container"
-      id="cardfixlogin"
-    >
-      <form>
-        <div className="form-group">
-          <label for="formGroupExampleInput">Nome do Filme</label>
-          <input
-            type="text"
-            className="form-control"
-            id="formGroupExampleInput"
-          />
-        </div>
-        <div className="form-group">
-          <label for="formGroupExampleInput2">URL da imagem</label>
-          <input
-            type="text"
-            className="form-control"
-            id="formGroupExampleInput2"
-          />
-        </div>
-        <div class="form-group">
-          <label for="exampleFormControlTextarea1">Texto</label>
-          <textarea
-            class="form-control "
-            id="exampleFormControlTextarea1"
-            rows="30"
-          ></textarea>
-        </div>
-        <div class="row">
-          <div class="col">
-            <label for="formGroupExampleInput">Direção</label>
-            <input
-              type="text"
-              className="form-control"
-              id="formGroupExampleInput"
-            />
+import { auth } from "./../config_firebase";
 
-            <label for="formGroupExampleInput">Fotografia</label>
+class Novo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuth: false,
+      isAuthing: true,
+      user: null,
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({
+        isAuthing: false,
+        isAuth: !!user,
+        user,
+      });
+    });
+  }
+
+  render() {
+    if (this.state.isAuthing) {
+      return <p>espera ai...</p>;
+    }
+    if (!this.state.isAuth) {
+      return <Redirect to="login" />;
+    }
+    return (
+      <div
+        className="d-flex justify-content-right align-items-center container"
+        id="cardfixlogin"
+      >
+        <form>
+          <div className="form-group">
+            <label for="formGroupExampleInput">Nome do Filme</label>
             <input
               type="text"
               className="form-control"
               id="formGroupExampleInput"
             />
           </div>
-          <div class="col">
-            <label for="formGroupExampleInput">Roteiro</label>
+          <div className="form-group">
+            <label for="formGroupExampleInput2">URL da imagem</label>
             <input
               type="text"
               className="form-control"
-              id="formGroupExampleInput"
-            />
-            <label for="formGroupExampleInput">Atuação</label>
-            <input
-              type="text"
-              className="form-control"
-              id="formGroupExampleInput"
+              id="formGroupExampleInput2"
             />
           </div>
-        </div>
-        <button type="button" className="btn btn-success mt-4">
-          Enviar
-        </button>
-      </form>
-    </div>
-  );
-};
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Texto</label>
+            <textarea
+              class="form-control "
+              id="exampleFormControlTextarea1"
+              rows="30"
+            ></textarea>
+          </div>
+          <div class="row">
+            <div class="col">
+              <label for="formGroupExampleInput">Direção</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formGroupExampleInput"
+              />
+
+              <label for="formGroupExampleInput">Fotografia</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formGroupExampleInput"
+              />
+            </div>
+            <div class="col">
+              <label for="formGroupExampleInput">Roteiro</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formGroupExampleInput"
+              />
+              <label for="formGroupExampleInput">Atuação</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formGroupExampleInput"
+              />
+            </div>
+          </div>
+          <button type="button" className="btn btn-success mt-4">
+            Enviar
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Novo;
